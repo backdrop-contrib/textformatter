bcf403e78364398423836f3170718ee33dcab918
Damian Lee <damian@damoweb.co.uk>
2012-03-03 20:58:15 +0000

    Code tidy, change of func/var names, Additional comments

--- textformatter.api.php
+++ textformatter.api.php
@@ -1,26 +1,32 @@
 <?php
 
 /**
- * hook_textformatter_field_info().
+ * hook_textformatter_field_list_info().
  *
  * Declare new field types/callbacks that are available as text formatter lists.
- *
  */
-function hook_textformatter_field_info() {
-  $fields = array();
+function hook_textformatter_field_list_info() {
+  $info = array();
 
-  $fields['example'] = array( // key array with module name.
+  $info['example'] = array( // key array with module name.
     'fields' => array('example', 'example_other'), // This must be an array.
     // Callback to process $items from hook_field_formatter_view.
-    'callback' => 'textformatter_example_fields_render',
+    'callback' => 'textformatter_example_field_create_list',
   );
 
-  return $fields;
+  return $info;
 }
 
 /**
  * Sample callback implementation.
+ * @see textformatter_default_field_create_list.
  */
-function textformatter_example_fields_render($items, $display, $settings) {
-  
+function textformatter_example_field_create_list($items, $field, $display, $settings) {
+  $list_items = array();
+
+  foreach ($items as $delta => $item) {
+    $list_items[$delta] = check_plain($item['value']);
+  }
+
+  return $list_items;
 }
